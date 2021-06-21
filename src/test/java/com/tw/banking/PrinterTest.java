f72bf85fd39297f0e6e1e41f5411e0e0920211fc
waylon <waylon.ye@oocl.com>
2021-06-21 21:23:47 +0800

    [Printer] should use console print transactions summary when print given transactions

--- src/test/java/com/tw/banking/PrinterTest.java
+++ src/test/java/com/tw/banking/PrinterTest.java
@@ -7,6 +7,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -36,6 +37,24 @@ class PrinterTest {
         Assertions.assertAll(
                 () -> verify(console, times(1)).printLine(Printer.STATEMENT_HEADER),
                 () -> verify(console, times(1)).printLine("21/06/2021 | 0 | 0")
+        );
+    }
+
+    @Test
+    void should_use_console_print_transactions_summary_when_print_given_transactions() {
+        // given
+        Transaction transaction1 = new Transaction("20/06/2021", 5);
+        Transaction transaction2 = new Transaction("21/06/2021", 100);
+        List<Transaction> transactions = Arrays.asList(transaction1, transaction2);
+
+        // when
+        printer.print(transactions);
+
+        // then
+        Assertions.assertAll(
+                () -> verify(console, times(1)).printLine(Printer.STATEMENT_HEADER),
+                () -> verify(console, times(1)).printLine("20/06/2021 | 5 | 5"),
+                () -> verify(console, times(1)).printLine("21/06/2021 | 100 | 105")
         );
     }
 }
