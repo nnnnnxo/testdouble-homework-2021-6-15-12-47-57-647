5bcc33556b9db9078e21813e3b94f3b99ba5ace3
waylon <waylon.ye@oocl.com>
2021-06-21 21:37:51 +0800

    [TransactionRepository] should add deposit transaction when add deposit given amount and today date

---
+++ src/test/java/com/tw/banking/TransactionRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.tw.banking;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class TransactionRepositoryTest {
+    @InjectMocks
+    private TransactionRepository transactionRepository;
+    @Mock
+    private Clock clock;
+
+    @Test
+    void should_add_deposit_transaction_when_add_deposit() {
+        // given
+        int amount = 100;
+        String today = "21/06/2021";
+        when(clock.todayAsString()).thenReturn(today);
+
+        // when
+        transactionRepository.addDeposit(amount);
+
+        // then
+        List<Transaction> result = transactionRepository.allTransactions();
+        Assertions.assertAll(
+                () -> assertEquals(1, result.size()),
+                () -> assertEquals(today, result.get(0).date()),
+                () -> assertEquals(amount, result.get(0).amount())
+
+        );
+    }
+}
