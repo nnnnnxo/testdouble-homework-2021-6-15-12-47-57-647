e903dd4c959c356c2c92ccff1885a0c1095ce537
waylon <waylon.ye@oocl.com>
2021-06-21 21:17:21 +0800

    [Printer] should use console print when print given transactions

---
+++ src/test/java/com/tw/banking/PrinterTest.java
@@ -0,0 +1,41 @@
+package com.tw.banking;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class PrinterTest {
+    @InjectMocks
+    private Printer printer;
+    @Mock
+    private Console console;
+
+    @Test
+    void should_use_console_print_when_print_given_transactions() {
+        // given
+        int amount = 0;
+        String date = "21/06/2021";
+
+        Transaction transaction = new Transaction(date, amount);
+        List<Transaction> transactions = Collections.singletonList(transaction);
+
+        // when
+        printer.print(transactions);
+
+        // then
+        Assertions.assertAll(
+                () -> verify(console, times(1)).printLine(Printer.STATEMENT_HEADER),
+                () -> verify(console, times(1)).printLine("21/06/2021 | 0 | 0")
+        );
+    }
+}
