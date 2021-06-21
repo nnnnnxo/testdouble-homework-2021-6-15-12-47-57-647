e0e095bbdf3c9caa3fd7a9cf855bb9a7bc1599f1
waylon <waylon.ye@oocl.com>
2021-06-21 21:02:32 +0800

    [Account] should add deposit transaction when add deposit

---
+++ src/test/java/com/tw/banking/AccountTest.java
@@ -0,0 +1,28 @@
+package com.tw.banking;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class AccountTest {
+    @InjectMocks private Account account;
+    @Mock private TransactionRepository transactionRepository;
+
+    @Test
+    void should_add_deposit_transaction_when_deposit() {
+        // given
+        int amount = 100;
+
+        // when
+        account.deposit(100);
+
+        // then
+        verify(transactionRepository, times(1)).addDeposit(amount);
+    }
+}
